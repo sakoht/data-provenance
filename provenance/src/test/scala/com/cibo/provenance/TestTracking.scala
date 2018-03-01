7a8eaed27356b50c1059082bfb48ab7b48d31207
Scott M. Smith <scottsmith1@gmail.com>
2018-03-01 20:45:03 +0800

    Update TestTracking.wipe for S3.

--- provenance/src/test/scala/com/cibo/provenance/TestTracking.scala
+++ provenance/src/test/scala/com/cibo/provenance/TestTracking.scala
@@ -11,10 +11,16 @@ trait TestTracking extends ResultTrackerSimple {
     */
   def wipe: Unit = {
     if (basePath.isRemote) {
-      val paths = s3db.getSuffixesForPrefix("")
-      paths.foreach {
-        path =>
-          println("DELETE: " + path)
+      sys.env.get("USER") match {
+        case Some(user) =>
+          if (!basePath.path.contains("-" + user))
+            throw new RuntimeException(f"Refusing to delete a test a directory that does not contain the current user's name: $user")
+          com.cibo.io.Shell.run(s"aws s3 rm --recursive ${basePath.path}")
+        case None =>
+          throw new RuntimeException(
+            "Failed to determine the current user." +
+            f"Refusing to delete a test a directory that does not contain the current user's name!"
+          )
       }
     } else {
       val testDataDir = basePath.getFile
