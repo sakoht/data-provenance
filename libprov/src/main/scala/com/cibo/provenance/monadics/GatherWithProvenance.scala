8f245752203d6a81450f979a6947654f9c512f5b
Scott M. Smith <scottsmith1@gmail.com>
2018-08-30 20:45:38 +0800

    Reduce GatherWithProvenance.{Call,Result}.

--- libprov/src/main/scala/com/cibo/provenance/monadics/GatherWithProvenance.scala
+++ libprov/src/main/scala/com/cibo/provenance/monadics/GatherWithProvenance.scala
@@ -16,7 +16,6 @@ import io.circe.{Decoder, Encoder}
 import com.cibo.provenance._
 import com.cibo.provenance.implicits.Traversable
 
-
 class GatherWithProvenance[S[_], E](
   implicit hok: Traversable[S],
   cda: Codec[E],
@@ -63,8 +62,10 @@ class GatherWithProvenance[S[_], E](
     implicit def decoder(implicit rt: ResultTracker): Decoder[Call] = new ValueWithProvenanceDecoder[S[E], Call]
   }
 
-  case class Call(in: S[ValueWithProvenance[E]], v: ValueWithProvenance[Version])
-    extends GatherCallWithProvenance[S, E](in, v)(implVersion) with Serializable {
+  case class Call(
+    gatheredInputs: S[ValueWithProvenance[E]],
+    v: ValueWithProvenance[Version]
+  ) extends FunctionCallWithProvenance[S[E]](v) with Serializable {
 
     val function: GatherWithProvenance[S, E] = self
 
@@ -82,6 +83,35 @@ class GatherWithProvenance[S[_], E](
     override def unresolve(implicit rt: ResultTracker): Call =
       unresolveInputs(rt).asInstanceOf[Call]
 
+    def inputs: Seq[ValueWithProvenance[E]] = hok.toSeq(gatheredInputs)
+
+    def inputTuple: Tuple1[S[ValueWithProvenance[E]]] = Tuple1(gatheredInputs)
+
+    def resolveInputs(implicit rt: ResultTracker): Call = {
+      def f(a: ValueWithProvenance[E]): ValueWithProvenance[E] = a.resolve
+      nocopy(duplicate(hok.map(f)(gatheredInputs), v.resolve), this)
+    }
+
+    def unresolveInputs(implicit rt: ResultTracker): Call = {
+      def f(a: ValueWithProvenance[E]): ValueWithProvenance[E] = a.unresolve
+      nocopy(duplicate(hok.map(f)(gatheredInputs), v.unresolve), this)
+    }
+
+    def loadInputs(implicit rt: ResultTracker): Call = {
+      def f(a: ValueWithProvenance[E]): ValueWithProvenance[E] = a.load
+      nocopy(duplicate(hok.map(f)(gatheredInputs), v.unresolve), this)
+    }
+
+    def loadRecurse(implicit rt: ResultTracker): Call = {
+      def f(a: ValueWithProvenance[E]): ValueWithProvenance[E] = a.loadRecurse
+      nocopy(duplicate(hok.map(f)(gatheredInputs), v.unresolve), this)
+    }
+
+    def saveInputs(implicit rt: ResultTracker): Call = {
+      def f(a: ValueWithProvenance[E]): ValueWithProvenance[E] = a.save
+      nocopy(duplicate(hok.map(f)(gatheredInputs), v.unresolve), this)
+    }
+
     def newResult(output: VirtualValue[S[E]])(implicit bi: BuildInfo): Result =
       Result(this, output)(bi)
 
@@ -90,17 +120,17 @@ class GatherWithProvenance[S[_], E](
   }
 
   case class Result(override val call: Call, override val outputAsVirtualValue: VirtualValue[S[E]])(implicit bi: BuildInfo)
-    extends GatherCallResultWithProvenance[S, E](call, outputAsVirtualValue)(bi)
+    extends FunctionCallResultWithProvenance[S[E]](call, outputAsVirtualValue, bi)
 
-  implicit private def convertCall(call: GatherCallWithProvenance[S, E]): Call =
+  implicit private def convertCall(call: Call): Call =
     Call(call.gatheredInputs, call.version)
 
-  implicit private def convertResult(r: GatherCallResultWithProvenance[S, E]): Result =
+  implicit private def convertResult(r: Result): Result =
     Result(r.call, r.outputAsVirtualValue)(r.outputBuildInfoBrief)
 }
 
 object GatherWithProvenance {
-  // Factory for type-specific Gatherers
+  // Generate a GatherWithProvenance function for the specific type.
   def apply[S[_], E]()(
     implicit hok: implicits.Traversable[S],
     e: Codec[E],
@@ -109,7 +139,7 @@ object GatherWithProvenance {
     new GatherWithProvenance[S, E]
   }
 
-  // Gather.Call in one step.
+  // Create a GatherWithProvenance.Call in one step.
   def apply[S[_], E](
     inputs: S[ValueWithProvenance[E]]
   )(implicit hok: implicits.Traversable[S],
@@ -120,60 +150,3 @@ object GatherWithProvenance {
     gatherer(inputs)
   }
 }
-
-abstract class GatherCallWithProvenance[S[_], E](
-  val gatheredInputs: S[ValueWithProvenance[E]],
-  v: ValueWithProvenance[Version]
-)(implVersion: (S[E], Version) => S[E]
-)(implicit val hok: implicits.Traversable[S],
-  elc: Codec[S[E]],
-  ec: Codec[E]
-) extends FunctionCallWithProvenance[S[E]](v) with Serializable {
-
-  def inputs: Seq[ValueWithProvenance[E]] = hok.toSeq(gatheredInputs)
-
-  def inputTuple: Tuple1[S[ValueWithProvenance[E]]] = Tuple1(gatheredInputs)
-
-  // NOTE: In most cases these methods are overridden by FunctionNWithProvenance.Call to return
-  // the same object, but with a more specific known type.
-
-  def resolveInputs(implicit rt: ResultTracker): GatherCallWithProvenance[S, E] = {
-    def f(a: ValueWithProvenance[E]): ValueWithProvenance[E] = a.resolve
-    nocopy(duplicate(hok.map(f)(gatheredInputs), v.resolve), this)
-  }
-
-  def unresolveInputs(implicit rt: ResultTracker): GatherCallWithProvenance[S, E] = {
-    def f(a: ValueWithProvenance[E]): ValueWithProvenance[E] = a.unresolve
-    nocopy(duplicate(hok.map(f)(gatheredInputs), v.unresolve), this)
-  }
-
-  def loadInputs(implicit rt: ResultTracker): GatherCallWithProvenance[S, E] = {
-    def f(a: ValueWithProvenance[E]): ValueWithProvenance[E] = a.load
-    nocopy(duplicate(hok.map(f)(gatheredInputs), v.unresolve), this)
-  }
-
-  def loadRecurse(implicit rt: ResultTracker): GatherCallWithProvenance[S, E] = {
-    def f(a: ValueWithProvenance[E]): ValueWithProvenance[E] = a.loadRecurse
-    nocopy(duplicate(hok.map(f)(gatheredInputs), v.unresolve), this)
-  }
-
-  def saveInputs(implicit rt: ResultTracker): GatherCallWithProvenance[S, E] = {
-    def f(a: ValueWithProvenance[E]): ValueWithProvenance[E] = a.save
-    nocopy(duplicate(hok.map(f)(gatheredInputs), v.unresolve), this)
-  }
-
-  def run(implicit rt: ResultTracker): GatherCallResultWithProvenance[S, E]
-
-  def newResult(value: VirtualValue[S[E]])(implicit bi: BuildInfo): GatherCallResultWithProvenance[S, E]
-
-  // NOTE: This is implemented as Product.copy in the case-class subclasses.
-  def duplicate(inputs: S[ValueWithProvenance[E]] = gatheredInputs, v: ValueWithProvenance[Version] = v): GatherCallWithProvenance[S, E]
-}
-
-
-class GatherCallResultWithProvenance[S[_], E](
-  override val call: GatherCallWithProvenance[S, E],
-  override val outputAsVirtualValue: VirtualValue[S[E]]
-)(implicit bi: BuildInfo)
-  extends FunctionCallResultWithProvenance[S[E]](call, outputAsVirtualValue, bi)
-    with Serializable
