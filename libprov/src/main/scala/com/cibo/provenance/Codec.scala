6c48e114121aee7cd91d037eb72585f30e17e599
Scott M. Smith <scottsmith1@gmail.com>
2018-11-21 02:01:49 +0800

    Cosmetic cleanup of Codec[T] trait.

--- libprov/src/main/scala/com/cibo/provenance/Codec.scala
+++ libprov/src/main/scala/com/cibo/provenance/Codec.scala
@@ -12,20 +12,16 @@ import scala.reflect.runtime.universe.TypeTag
 /**
   * The base trait for wrapping encoder/decoder pairs.
   *
-  * The builtin encoding (JsonCodec) uses Circe JSON, but any subcass that convert to/from an Array[Byte] works.
+  * The builtin encoding (JsonCodec) uses Circe JSON is used exclusively by the core library,
+  * but any logic that can convert to/from an Array[Byte] works.
   *
   * @tparam T       The type of data to be encoded/decoded.
   */
 trait Codec[T] extends Serializable {
-  
   def classTag: ClassTag[T]
-
   def typeTag: TypeTag[T]
-
   def serializableClassName: String = Codec.classTagToSerializableName(classTag)
-
   def serialize(obj: T, checkConsistency: Boolean = true): Array[Byte]
-
   def deserialize(bytes: Array[Byte]): T
 }
 
